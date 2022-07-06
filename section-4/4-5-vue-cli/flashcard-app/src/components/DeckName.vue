585d4b969db3a2c774fc54a10cb8bdabd8e6a547
David Whipple <dwhipp88@gmail.com>
2022-07-05 18:41:29 -0600

    completed course

---
+++ section-4/4-5-vue-cli/flashcard-app/src/components/DeckName.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'DeckName',
+        props: ['name']
+    };
+</script>
+
+<style scoped>
+    /* 
+    You don't need to use style but it's a great place for component specific css
+    and it cannot make it really easy to update this component later because we'll
+    have the mark up, logic, and relevant CSS, all in one place.
+    */
+    h1 {
+        font-style: italic;
+    }
+</style>
